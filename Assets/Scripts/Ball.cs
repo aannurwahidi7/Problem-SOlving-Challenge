385ca119eac7029c65dd045c0cb935eca8f6968d
Aan Nur Wahidi <aannurwahidi117@gmail.com>
2021-10-09 19:22:07 +0700

    Add Problem 4

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
@@ -8,6 +9,11 @@ public class Ball : MonoBehaviour
     private float speed;
 
     private Rigidbody2D rigidbody2D;
+    private CircleCollider2D circleCollider;
+
+    private Vector2 movement;
+
+    public string SCENE_NAME;
 
     public float xForce;
     public float yForce;
@@ -16,14 +22,20 @@ public class Ball : MonoBehaviour
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
 
-        BallControl();
-    }
+        SCENE_NAME = SceneManager.GetActiveScene().name;        
 
-    // Update is called once per frame
-    void Update()
-    {
+        if(SCENE_NAME == "Problem 2" || SCENE_NAME == "Problem 3")
+        {
+            rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
+            BallControl();
+        }
         
+        else
+        {
+            circleCollider.sharedMaterial = null;
+        }
     }
 
     private void BallControl()
@@ -49,7 +61,34 @@ public class Ball : MonoBehaviour
                 // Memberi gaya pada pojok kiri bawah
                 rigidbody2D.AddForce(new Vector2(-xForce, -yForce));
                 break;
+        }               
+    }
+
+    private void FixedUpdate()
+    {
+        //Mendapatkan nilai input horizontal (-1,0,1)
+        float h = Input.GetAxisRaw("Horizontal");
+
+        //Mendapatkan nilai input vertical (-1,0,1)
+        float v = Input.GetAxisRaw("Vertical");
+
+        switch (SCENE_NAME)
+        {
+            case "Problem 4":
+                Move(h, v);
+                break;
         }
-       
+    }
+
+    public void Move(float h, float v)
+    {
+        //Set nilai x dan y
+        movement.Set(h, v);
+
+        //Menormalisasi nilai vector agar total panjang dari vector adalah 1
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        //Move to position
+        rigidbody2D.MovePosition(rigidbody2D.position + movement);
     }
 }
